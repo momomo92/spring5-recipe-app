32ec91165f5b98e0aedba2486f3f8a2020bc43da
NeMoMo <piotr@theworlt.com>
2021-01-29 23:57:33 +0100

    add unit of measure entity

--- src/main/java/momo/springframework/spring5recipeapp/domain/Ingredient.java
+++ src/main/java/momo/springframework/spring5recipeapp/domain/Ingredient.java
@@ -12,9 +12,11 @@ public class Ingredient {
 
     private BigDecimal amount;
     private String description;
-//    private UnitOfMeasure uam;
     @ManyToOne
     private Recipe recipe;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
 
     public Long getId() {
         return id;
@@ -46,5 +48,13 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 }
