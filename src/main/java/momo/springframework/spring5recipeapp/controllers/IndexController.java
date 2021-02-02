b9a1734beeaa49f28f9443629a79e1e8bf6fadef
NeMoMo <piotr@theworlt.com>
2021-02-02 21:51:37 +0100

    140. Spring Data JPA Query Methods

--- src/main/java/momo/springframework/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/momo/springframework/spring5recipeapp/controllers/IndexController.java
@@ -1,13 +1,33 @@
 package momo.springframework.spring5recipeapp.controllers;
 
+import momo.springframework.spring5recipeapp.domain.Category;
+import momo.springframework.spring5recipeapp.domain.UnitOfMeasure;
+import momo.springframework.spring5recipeapp.repositories.CategoryRepository;
+import momo.springframework.spring5recipeapp.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndexPag() {
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Category id is: " + categoryOptional.get().getId());
+        System.out.println("Unit of measure id is: " + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
