578f3cafaa37dee1e5801090c61b8c50a2ce8ea4
NeMoMo <piotr@theworlt.com>
2021-02-10 23:30:54 +0100

    174. Assignment - Write Test for IndexController

---
+++ src/test/java/momo/springframework/spring5recipeapp/controllers/IndexControllerTest.java
@@ -0,0 +1,33 @@
+package momo.springframework.spring5recipeapp.controllers;
+
+import momo.springframework.spring5recipeapp.service.RecipeServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IndexControllerTest {
+
+    IndexController indexController;
+
+    @Mock
+    Model model;
+
+    @Mock
+    RecipeServiceImpl recipeService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    void getIndexPag() {
+        assertEquals(indexController.getIndexPag(model), "index");
+    }
+}
