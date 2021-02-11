4a4afb284db1b6bc9f90a12ee35c5e892685bfa4
NeMoMo <piotr@theworlt.com>
2021-02-11 22:21:18 +0100

    176. Mockito Argument Capture

--- src/test/java/momo/springframework/spring5recipeapp/controllers/IndexControllerTest.java
+++ src/test/java/momo/springframework/spring5recipeapp/controllers/IndexControllerTest.java
@@ -1,12 +1,17 @@
 package momo.springframework.spring5recipeapp.controllers;
 
+import momo.springframework.spring5recipeapp.domain.Recipe;
 import momo.springframework.spring5recipeapp.service.RecipeServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anySet;
@@ -31,9 +36,27 @@ class IndexControllerTest {
 
     @Test
     void getIndexPag() {
+        //given
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+
+        Recipe recipe = new Recipe();
+        recipe.setId(4L);
+
+        recipes.add(recipe);
+
+        Mockito.when(recipeService.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        //when
         assertEquals("index", indexController.getIndexPag(model));
 
+        //then
         Mockito.verify(recipeService, Mockito.times(1)).getRecipes();
-        Mockito.verify(model, Mockito.times(1)).addAttribute(eq("recipes"), anySet());
+        Mockito.verify(model, Mockito.times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
     }
 }
